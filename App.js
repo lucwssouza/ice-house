28b3a34a5800f4b3596df86178f3a867d2ac2752
Lucas <141728920+lucwssouza@users.noreply.github.com>
2024-03-15 08:01:02 -0300

    a

--- App.js
+++ App.js
@@ -15,6 +15,7 @@ export default function App() {
           <Image
             style={styles.menuIcon}
             source={require("./assets/menu-icon.png")}
+
           />
         </TouchableOpacity>
         <Image
